2edcd8924c40919b9a15eb1ba21f37833b3c05a6
pavani <pavaniavni456@gmail.com>
2022-02-04 15:04:45 +0530

    initial commit

---
+++ structstudent/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace StructClass
+{
+    
+    public struct Student
+    {
+        public string Name;
+        public string Class;
+        public string Gender;
+        public void GetData()
+        {
+            Console.WriteLine("Enter student name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Class of the student: ");
+            Class = Console.ReadLine();
+            Console.WriteLine("Enter the gender of the student");
+            Gender = Console.ReadLine();
+
+        }
+        public void ShowData()
+        {
+            Console.WriteLine("The Name of student is " + Name);
+            Console.WriteLine("The class of student is " + Class);
+            Console.WriteLine("The gender of student is " + Gender);
+        }
+
+    }
+
+    class Structvsenum
+    {
+        
+        
+        public static void Main()
+        {
+            Student student=new Student();
+            student.GetData();
+            student.ShowData();
+            
+
+    
+        }
+    }
+}
